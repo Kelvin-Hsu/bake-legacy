1674e6fdf9a5464e54ff9bb20bbfe7cfbcc326aa
Kelvin Hsu <Kelvin.Hsu@nicta.com.au>
2016-07-06 18:07:37 +1000

    Added my linear kernel bayes rule

--- bake/kbr.py
+++ bake/kbr.py
@@ -4,7 +4,7 @@ Kernel Bayes Rule.
 import numpy as np
 from .linalg import solve_posdef
 
-def posterior_weight_matrix(prior_embedding_vector, k_xx, k_yy, epsil, delta):
+def posterior_weights_quadratic(mu_y_prior, k_xx, k_yy, epsil, delta):
     """
     Obtain the posterior weights involved in Kernel Bayes' Rule.
 
@@ -12,8 +12,8 @@ def posterior_weight_matrix(prior_embedding_vector, k_xx, k_yy, epsil, delta):
 
     Parameters
     ----------
-    prior_embedding : numpy.ndarray
-        The kernel embedding of the prior probability measure
+    mu_y_prior : numpy.ndarray
+        The kernel embedding of the prior probability measure (n)
     k_xx : numpy.ndarray
         The gram matrix on the observed input variables (n x n)
     k_yy : numpy.ndarray
@@ -29,13 +29,13 @@ def posterior_weight_matrix(prior_embedding_vector, k_xx, k_yy, epsil, delta):
         input x values and queried at arbitrary output y values (n x n) 
     """
     # [Data Size] n: scalar
-    n = prior_embedding_vector.shape[0]
+    n = mu_y_prior.shape[0]
 
     # [Identity] I: (n x n)
     I = np.eye(n)
 
     # [Prior Effect] prior_effect: (n x n)
-    prior_effect = np.diag(solve_posdef(k_yy + n * epsil * I, prior_embedding_vector)[0])
+    prior_effect = np.diag(solve_posdef(k_yy + n * epsil * I, mu_y_prior)[0])
 
     # [Observation Prior] obs_prior: (n x n)
     obs_prior = np.dot(prior_effect, k_xx)
@@ -45,3 +45,42 @@ def posterior_weight_matrix(prior_embedding_vector, k_xx, k_yy, epsil, delta):
 
     # [Posterior Weights] (n x n)
     return np.dot(obs_prior, solve_posdef(reg_sq_obs_prior, prior_effect)[0])
+
+def posterior_weights_linear(mu_y_prior, k_xx, k_yy, epsil, delta):
+    """
+    Obtain the posterior weights involved in Kernel Bayes' Rule.
+
+    The posterior refers to the posterior distribution of y given x.
+
+    Parameters
+    ----------
+    mu_y_prior : numpy.ndarray
+        The kernel embedding of the prior probability measure (n)
+    k_xx : numpy.ndarray
+        The gram matrix on the observed input variables (n x n)
+    k_yy : numpy.ndarray
+        The gram matrix on the observed output variables (n x n)
+    epsil : numpy.float64
+        The regularisation parameter for the prior effect
+    delta : numpy.float64
+        The reguarlisation parameter for the kernel Bayes' rule
+    Returns
+    -------
+    numpy.ndarray
+        The posterior weights ready to be conditioned on arbitrary 
+        input x values and queried at arbitrary output y values (n x n) 
+    """
+    # [Data Size] n: scalar
+    n, = mu_y_prior.shape
+
+    # [Identity] I: (n x n)
+    I = np.eye(n)
+
+    # [Prior Effect] d_y_prior: (n x n)
+    d_y_prior = np.diag(solve_posdef(k_yy + epsil * I, mu_y_prior)[0])
+
+    # [Posterior Weights] v: (n x n)
+    v = solve_posdef(np.dot(d_y_prior, k_xx) + (delta / n) * I, d_y_prior)[0]
+
+    # Return the posterior weights
+    return v
