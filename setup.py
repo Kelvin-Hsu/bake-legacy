8823bd712ac754638517706175216e10d16f20e1
darrnshn <darren.shen@sydney.edu.au>
2016-07-28 16:00:51 +1000

    Add simple setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bake',
+    version='0.0.0',
+    url='https://github.com/KelvyHsu/bake/',
+    author='Kelvin Hsu',
+    author_email='Kelvin.Hsu@data61.csiro.au',
+    install_requires=['numpy', 'scipy']
+)
