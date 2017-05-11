41be7bbc2852d3960dcdf9d662c20529bc672e12
KelvyHsu <Kelvin.Hsu@data61.csiro.au>
2017-05-11 11:10:08 +1000

    Add stationary kernel embedding classifier

--- cake/__init__.py
+++ cake/__init__.py
@@ -2,3 +2,4 @@ from . import kernels
 from .base_classifier import *
 from .linear_classifier import *
 from .deep_linear_classifiers import *
+from .stationary_kernel_embedding_classifier import StationaryKernelEmbeddingClassifier
